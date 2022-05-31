4f80ef489cf2200b249dfeaa8edf4964843aae1d
ittp <61617660+ittp@users.noreply.github.com>
2022-05-31 10:07:37 +0300

    Create api-template.js

---
+++ api/api-template.js
@@ -0,0 +1,19 @@
+
+// S:CS = strapi core services
+const { create, update, delete, find, findOne } = S.CS
+
+
+const A = [T, R, S, P, M, E]
+
+
+const api = {
+  types: {},
+  controllers: {
+   
+  },
+  routes: {},
+  services: {},
+  plugins: {},
+  middlewares: {},
+  configs: {}
+}
