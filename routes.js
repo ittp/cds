f13d4418f79e90cbfbce3df703966432f914538e
ittp <61617660+ittp@users.noreply.github.com>
2022-05-31 13:47:46 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,3 +1,10 @@
+let route = (c) = {
+let basedir = ""
+method: c.method.toUpperCase(), path: path.join(basedir, c.path), handler: c.handler, config: c.config
+
+}
+let connect = (request) => { method: "post", handler: bot.services.connect, config: {} }
+
 const routes = {
-  
+  connect
 }
