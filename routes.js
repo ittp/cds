4dbf5a649dbc4076bf3e4377df0d3d3b457cba5e
ittp <61617660+ittp@users.noreply.github.com>
2022-05-31 13:52:58 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,10 +1,34 @@
-let route = (c) = {
-let basedir = ""
-method: c.method.toUpperCase(), path: path.join(basedir, c.path), handler: c.handler, config: c.config
+
+
+let bot = {
+  controllers: {},
+  
+ services: 
+{
+  connect: {
+  
+  cid: "number",
+  token: "string",
+  
+  },
+  command: {},
+  message: {},
+  reply: {},
+  webhook: {},
+  updates:{}
+}
 
 }
-let connect = (request) => { method: "post", handler: bot.services.connect, config: {} }
+
+let connect = (request) => { method: "post", handler: bot.services.connect, params: [ "cid", "token" ] }
+let command = (ctx) => { method: "post", handler: bot.services.command, params: [ "command" ] }
+let message = (ctx) => { method: "post", handler: bot.services.message, params: ["chat_id", "data"] }
+let reply = (ctx) => { method: "post", handler: bot.services.reply, params: ["chat_id", "text", "buttons"] }
+
 
 const routes = {
-  connect
+  connect,
+  command,
+  message,
+  reply
 }
